b2753c1e118ea552fbed8c68b40ca6787b49ce37
Ivan Yatsenko <aokiroyal16@gmail.com>
2023-06-17 18:36:46 +0300

    Init

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Homeworks_WPF
 {
@@ -20,9 +22,71 @@ namespace Homeworks_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static string data_path = "data.bin";
         public MainWindow()
         {
             InitializeComponent();
+            ReadItems();
+        }
+        private void ReadItems()
+        {
+            FileStream fs = File.Open(data_path, FileMode.Open, FileAccess.Read);
+            BinaryFormatter bf = new BinaryFormatter();
+            while (fs.Position < fs.Length)
+            {
+                data.Items.Add(bf.Deserialize(fs));
+            }
+            fs.Close();
+        }
+        private void WriteItem(Task task)
+        {
+            FileStream fs = File.Open(data_path, FileMode.OpenOrCreate, FileAccess.Write);
+            BinaryFormatter bf = new BinaryFormatter();
+            foreach (object i in data.Items)
+            {
+                bf.Serialize(fs,i as Task);
+            }
+            fs.Close();
+        }
+        private void AddItem(object sender, RoutedEventArgs e)
+        {
+            if (tb_name.Text.Length == 0)
+            {
+                MessageBox.Show("Name cannot be empty!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            int yy;
+            int mm;
+            int dd;
+            int hours;
+            int minutes;
+            try
+            {
+                dd = int.Parse(tb_days.Text);
+                mm = int.Parse(tb_months.Text);
+                yy = int.Parse(tb_years.Text);
+                DateTime temp = new DateTime(yy, mm, dd);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Date value is invalid!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            try
+            {
+                hours = int.Parse(tb_hours.Text);
+                minutes = int.Parse(tb_minutes.Text);
+                DateTime temp = new DateTime(1, 1, 1,hours,minutes,0);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Time value is invalid!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            DateTime date = new DateTime(yy, mm, dd, hours, minutes, 0);
+            Task task = new Task(tb_name.Text, date);
+            data.Items.Add(task);
+            WriteItem(task);
         }
     }
 }
---
+++ Tasks.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homeworks_WPF
+{
+    [Serializable]
+    internal class Task
+    {
+        private string name;
+        private DateTime date;
+        public Task(string name, DateTime date)
+        {
+            this.name = name;
+            this.date = date;
+        }
+        public string Name
+        {
+            get { return name; }
+        }
+        public DateTime Date
+        {
+            get { return date; }
+        }
+    }
+}
